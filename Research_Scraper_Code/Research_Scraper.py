ff269cd25cf114fdf2a34c8523cc5868d45a5a1e
Leon Cena <leon.cena@uni-muenster.de>
2022-10-14 13:39:35 +0200

    utils integration, doi link resolver logic

--- Research_Scraper_Code/Research_Scraper.py
+++ Research_Scraper_Code/Research_Scraper.py
@@ -3,6 +3,7 @@ import re
 from Research_Scraper_Code.scraper_types.scraper_ieee import ScraperIEEE
 from Research_Scraper_Code.scraper_types.scraper_sciencedirect import ScraperScienceDirect
 from Research_Scraper_Code.scraper_types.scraper_springer import ScraperSpringer
+from Research_Scraper_Code import utils
 
 
 class ResearchScraper:
@@ -16,11 +17,11 @@ class ResearchScraper:
         self.__check_params_type(params)
 
         # todo doi regex
-        if self.check_if_doi_link(url):
-            print(f'URL (\'{url}\') is a DOI link, please resolve DOI link first otherwise the scraper will not work '
+        if utils.check_if_doi_link(url):
+            print(f'URL (\'{url}\') is a DOI link, Links is now resolved '
                   f'properly')
-            # todo call doi resolver here
-            return None  # until doi resolver is implemented
+            url = utils.resolve_url(url)
+            print(f'Resolved DOI link to: {url}')
 
         result = {}
 
@@ -40,37 +41,3 @@ class ResearchScraper:
         for param in params:
             if not isinstance(param, str):
                 raise Exception('"Params" must consist only of strings')
-
-    # todo move later to utils
-    def check_if_doi_link(self, url):
-        doi_link_regex = re.compile(r'^(https?:\/\/)?'  # A doi links starts with http:// or https://
-                                    r'(www\.)?'  # or start with www.
-                                    r'(doi\.org\/)'  # followed by doi.org/
-                                    r'([a-zA-Z0-9\-\.\_]+'  # prefix: any character, number, - or . or _ any number of times
-                                    r'\/[a-zA-Z0-9\-\.\_]+)'  # suffix: any character, number, - or . or _ any number of times
-                                    r'$')
-
-        if doi_link_regex.match(url):
-            return True
-        else:
-            return False
-
-
-url_ieee = 'https://ieeexplore.ieee.org/document/7887648'
-url_springer = 'https://link.springer.com/article/10.1007/s12525-020-00445-0'
-url_doi = 'https://doi.org/10.1007/978-0-387-73947-2_8'
-
-test_urls = [url_ieee, url_springer]
-
-researchscraper = ResearchScraper()
-
-if False:
-    print(
-        f'Testing scraping the doi link {url_doi} > Result: {researchscraper.scrape_publication_by_url(url_doi, ["title", "authors"])}')
-
-if True:
-    test_url = url_springer
-    result = researchscraper.scrape_publication_by_url(test_url, params=['full'])
-    print('Test Start for: ', test_url, '\n______________________________________________________')
-    for key, value in result.items():
-        print(key, ': \n', '> ', value, '\n')
